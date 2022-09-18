698a40dea93d5e0cefd141f0a309c79bef007820
Imbris <imbrisf@gmail.com>
2022-09-18 14:55:58 -0400

    Remove env vars from launcher.sh that will be set by airshipper anyway.

--- snap/local/launcher/launcher.sh
+++ snap/local/launcher/launcher.sh
@@ -1,12 +1,9 @@
 #!/bin/bash
 
-export VELOREN_USERDATA=$SNAP_USER_DATA
 # Explicitly override VOXYGEN_LOGS because otherwise
 # Veloren saves invalid log paths from previous snap revisions
 # in its settings file.
 export VOXYGEN_LOGS=$SNAP_USER_DATA/voxygen/logs
-# Point veloren to its assets folder
-export VELOREN_ASSETS=$SNAP/usr/share/veloren/assets
 
 # Export the Vulkan ICD filename paths
 # Based on the retroarch snap's Vulkan setup (https://github.com/libretro/retroarch-snap)
